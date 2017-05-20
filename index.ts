06c589311fa6f346fdf4a715079b9e3deaa429f6
jasonHzq <hzongquan@126.com>
2017-05-20 18:48:55 +0800

    feat: add defaultProps to refect options type define

--- index.ts
+++ index.ts
@@ -78,6 +78,7 @@ interface Options {
   mapStateToProps?: (storeState?: any, props?: any, storeAllState?: any) => any;
   Effects?: BaseEffect<any>[]
   defaultNamespace: string
+  defaultProps: any
   View: any
 }
 
